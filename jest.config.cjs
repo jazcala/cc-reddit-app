34fa580a0ef9c9ebb5e44e38026b4ca008d1d363
jazcala <jazcalabrese@gmail.com>
2025-02-22 16:25:04 -0300

    fix setupFilesAfterEnv in jest config and fix  setupTest to not use imports

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.cjs"],
   verbose: true,
   testPathIgnorePatterns: ["/node_modules/"],
   moduleFileExtensions: ["js", "jsx", "json", "node"],
