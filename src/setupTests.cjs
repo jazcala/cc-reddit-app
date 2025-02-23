efb78cb56be876cdf671bd2a2ba16c0479877cdb
jazcala <jazcalabrese@gmail.com>
2025-02-23 14:22:30 -0300

    Fixing merge issues

--- src/setupTests.cjs
+++
@@ -1,8 +0,0 @@
-import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from "node:util";
-
-if (typeof global.TextEncoder === "undefined") {
-  global.TextEncoder = TextEncoder;
-  global.TextDecoder = TextDecoder;
-  console.log("TextEncoder polyfilled!");
-}
