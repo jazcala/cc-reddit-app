c7edb1a455bd5fde6f53bd49cdb018632809fcd5
jazcala <jazcalabrese@gmail.com>
2025-02-28 19:04:15 -0300

    Fixing styles in comment list adding heart icon for comment votes

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -14,14 +14,14 @@ const Comment = ({ comment }) => {
   }
 
   return (
-    <li>
+    <li className="list-group-item border-0 p-0">
       <div className="card p-0 my-2 mx-0">
         <div className="card-body p-0 ">
-          <div className="row bg-body-secondary m-0">
+          <div className="row bg-body-secondary m-0 pt-2">
             <h5 className="card-title text-start col-8">{author}</h5>
-            <p className=" text-end col-4">{score}</p>
+            <p className=" text-end col-4"><i class="bi bi-heart-fill text-danger"></i> {score}</p>
           </div>
-          <p className="card-text text-start">{body}</p>
+          <p className="card-text text-start p-2 ">{body}</p>
         </div>
       </div>
     </li>
