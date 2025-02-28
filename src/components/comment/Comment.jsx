ed94b88b03370180a31ffa96b2ffc15795c6b41e
jazcala <jazcalabrese@gmail.com>
2025-02-28 10:10:33 -0300

    Remove reactstrap from comments and update some styles

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -8,16 +8,23 @@ import PropTypes from "prop-types";
 
 const Comment = ({ comment }) => {
   const { author, score, body } = comment;
+
+  if( !author ){
+    return <></>
+  }
+
   return (
-    <div className="card p-0 my-2 mx-0">
-      <div className="card-body p-0 ">
-        <div className="row bg-body-secondary m-0">
-          <h5 className="card-title text-start col-8">{author}</h5>
-          <p className=" text-end col-4">{score}</p>
+    <li>
+      <div className="card p-0 my-2 mx-0">
+        <div className="card-body p-0 ">
+          <div className="row bg-body-secondary m-0">
+            <h5 className="card-title text-start col-8">{author}</h5>
+            <p className=" text-end col-4">{score}</p>
+          </div>
+          <p className="card-text text-start">{body}</p>
         </div>
-        <p className="card-text text-start">{body}</p>
       </div>
-    </div>
+    </li>
   );
 };
 
